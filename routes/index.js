6a27b5678afac0f751697429eeb748a8730db180
Sprazzeus <sprazzeus@gmail.com>
2016-08-08 12:06:45 +0800

    new goal route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
-var applicationController = require('../controllers/user_controller')
+var userController = require('../controllers/user_controller')
+var goalController = require('../controllers/goal_controller')
 var User = require('../models/user')
 const bodyParser = require('body-parser')
 router.use(bodyParser.json())
@@ -11,18 +12,20 @@ router.get('/', function (req, res, next) {
   res.render('../views/index.ejs')
 })
 
+router.post('/newgoal', goalController.newGoal)
+
 /* POST new user */
-router.post('/register', applicationController.userRegister)
+router.post('/register', userController.userRegister)
 
 /* EDIT new user get started (after registration) */
-router.post('/getstarted', applicationController.editUser)
+router.post('/getstarted', userController.editUser)
 /* EDIT user account */
-router.put('/account', applicationController.editUser)
+router.put('/account', userController.editUser)
 
 // User LOGIN
-router.post('/login', applicationController.userLogIn)
+router.post('/login', userController.userLogIn)
 
 // posts !!AND!! user DELETE
-router.delete('/deleteUser', applicationController.deleteUser)
+router.delete('/deleteUser', userController.deleteUser)
 
 module.exports = router
